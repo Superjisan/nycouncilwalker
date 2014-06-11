f9b200502cc190dbc528dfe092a61054c78c5eae
Jisan Zaman <jzthegreat@gmail.com>
2014-06-11 00:11:34 -0400

    added jquery script to enable proper active navbar links

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-var cheerio = require('cheerio'),
-    request = require('request'),
+var request = require('request'),
     express = require('express'),
     async = require('async'),
     path = require('path'),
@@ -8,66 +7,12 @@ var cheerio = require('cheerio'),
     sass = require('node-sass'),
     swig = require('swig');
 
-var baseUrl = "http://council.nyc.gov/html/members/members.shtml";
-var startingUrl = baseUrl;
-
-var council_member = function(attrs){
-  this.link = attrs.url;
-  this.name = attrs.name;
-  this.district = attrs.district;
-  this.borough = attrs.borough;
-  this.party = attrs.party;
-  this.img = attrs.img
-};
-
-var all_cm = {}
-
-var pageHtml = request({uri: baseUrl}, function(err, response, body) {
-
-    console.log("Crawling URL: ", baseUrl)
-
-    //Just a basic error check
-    if(err && response.statusCode !== 200){
-        console.log('Request error.');
-      }
-
-    var $ = cheerio.load(body);
-    // let's stop the engine here and
-    // debugger;
-
-    var council_members_raw_data = $(".list_entry").text()
-    // var links = $("a").filter(function(i,link){
-    //   return $(link).attr('href').match(/d/)
-    // }).map(function(i, link){
-    //    var url  = $(link).attr('href').match(/d\(/\d+/$/)
-    //    return url
-    // })
-    // console.log( links)
-
-    var council_member = {
-      name : "",
-      district : 0,
-      borough : 0,
-      party : "Independent"
-    }
-
-    var cm_array = council_members_raw_data.split("Democrat" || "Republican")
-    cm_array.map(function(elem){
-      if (elem.indexOf("Republican") > -1 ) {
-        // console.log("found a republican", elem)
-        elem.split("Republican")
-        }
-      }
-    )
-    console.log(cm_array )
-});
-
-
 var app = express();
 
-app.set('port', 3333);
+app.set('port', 3000);
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
+swig.setDefaults({ cache: false });
 app.set('views', path.join(__dirname, 'views'));
 swig.setDefaults({ cache: false });
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
