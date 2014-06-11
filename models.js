ccf8a273ea4061372737d5d6e611b53e969788d0
Jisan Zaman <jzthegreat@gmail.com>
2014-06-10 23:00:29 -0400

    setup councilmember schema

--- models.js
+++ models.js
@@ -1,2 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+var CouncilMemberSchema = new Schema ({
+  name : String,
+  district_number: Number,
+  borough: String,
+  party : String
+})
+
+var CouncilMember = mongoose.model('CouncilMember', CouncilMemberSchema)
+
+module.exports = {"CouncilMember": CouncilMember}
