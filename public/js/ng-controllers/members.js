c113a7004c210a0de1608567456332d86a1dead3
Jisan Zaman <jzthegreat@gmail.com>
2014-06-11 17:23:25 -0400

    Added Search Functionality by borough

--- public/js/ng-controllers/members.js
+++ public/js/ng-controllers/members.js
@@ -1,8 +1,14 @@
 'use strict';
 
-angular.module('nyccouncil').controller('MembersController', ['$scope', 'Global',
-  function ($scope, Global) {
+angular.module('nyccouncil').
+controller('MembersController',
+  ['$scope',
+    '$window',
+  'Global',
+  function ($scope, $window, Global) {
     $scope.global = Global;
+
+    $scope.members = $window.councilmembers;
 
 
 }]);
