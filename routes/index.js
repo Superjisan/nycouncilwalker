068487ac10a4a7dcdbad613a0a3913edc0386bd3
Jisan Zaman <jzthegreat@gmail.com>
2014-06-11 00:29:18 -0400

    got council data to the window

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
-
+var mongoose = require('mongoose')
+var models = require('../models')
+var CouncilMember = models.CouncilMember
 var async = require('async')
 
 exports.index = function(req, res) {
-  res.render('index')
+  CouncilMember.find({}, function(err, members){
+    if (err) { res.render('error', {status : 500})
+  } else {
+    console.log(members)
+    res.render('index', {council_members: members})
+    }
+  })
 }
