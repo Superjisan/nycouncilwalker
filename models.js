725f44a3c39e0ff44781590493cbe2b11eb33849
Jisan Zaman <jzthegreat@gmail.com>
2014-06-10 23:06:41 -0400

    test github commit

--- models.js
+++ models.js
@@ -11,3 +11,5 @@ var CouncilMemberSchema = new Schema ({
 var CouncilMember = mongoose.model('CouncilMember', CouncilMemberSchema)
 
 module.exports = {"CouncilMember": CouncilMember}
+
+//this is a comment to see if github commits work
