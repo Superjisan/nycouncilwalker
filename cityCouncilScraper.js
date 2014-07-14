2e0809e83154ba19ef1be70ff7ad215caaf2c35e
Jisan Zaman <jzthegreat@gmail.com>
2014-07-14 17:26:38 -0400

    got all data off of members page

--- cityCouncilScraper.js
+++ cityCouncilScraper.js
@@ -13,13 +13,58 @@ function scrapeCityCouncilURL(){
 
     var $ = cheerio.load(data);
 
-    var names = [];
-    var $names = $("#members_table .list_entry:first-child");
-    $names.each(function(i, elem){
-      names[i] = $(this).text();
-    })
+    var names = [], districts = [], boroughs = [], parties = [], urls = [];
 
-    console.log("names", names)
+    function scrapeEntryData(column, arraytoPush){
+      if(column === 'url'){
+        $values = $("#members_table .list_entry:first-child a")
+        // console.log("urls:", $values)
+        $values.each(function(i, elem){
+          arraytoPush[i] = $(this).attr().href;
+        })
+      } else {
+        var $values = $("#members_table .list_entry:nth-child("+column+")");
+        $values.each(function(i, elem){
+          arraytoPush[i] = $(this).text();
+        })
+      }
+    }
+
+    scrapeEntryData(1, names);
+    scrapeEntryData(2, districts);
+    scrapeEntryData(3, boroughs);
+    scrapeEntryData(4, parties);
+    scrapeEntryData('url', urls);
+
+    var citycouncilMembers = [];
+    for(var i = 0; i < names.length; i++){
+      var result = {};
+      result.name = names[i];
+      result.party = parties[i];
+      result.district = districts[i];
+      result.url = "http://council.nyc.gov"+urls[i];
+      result.borough = boroughs[i];
+      citycouncilMembers.push(result)
+    }
+
+    // console.log("names", names);
+    // console.log("districts", districts);
+    console.log("citycouncilMembers", citycouncilMembers);
+  })
+}
+
+function scrapeCouncilMemberPage(url){
+  var email, photo_url, offices = [];
+  request({url: url}, function(err, response, body){
+
+    console.log("scraping council member url:", url)
+    //Just a basic error check
+    if(err && response.statusCode !== 200){
+      console.log('Request error:', err);
+    }
+
+    var $ = cheerio.load(body);
+
   })
 }
 
