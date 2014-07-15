42a0101744ecf3066d985647610ecbf7f1c2776c
Jisan Zaman <jzthegreat@gmail.com>
2014-07-15 11:23:19 -0400

    got the right scraper, still need contact info though

--- cityCouncilScraper.js
+++ cityCouncilScraper.js
@@ -1,5 +1,9 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var async = require('async');
+var fs = require('fs');
+var util = require('util')
+var names = [], districts = [], boroughs = [], parties = [], urls = [], photo_urls=[], emails=[]; citycouncilMembers = {};
 
 function scrapeCityCouncilURL(){
   var url = "http://council.nyc.gov/html/members/members.shtml";
@@ -12,8 +16,6 @@ function scrapeCityCouncilURL(){
     }
 
     var $ = cheerio.load(data);
-
-    var names = [], districts = [], boroughs = [], parties = [], urls = [], photo_url;
 
     function scrapeEntryData(column, arraytoPush){
       if(column === 'url'){
@@ -36,7 +38,6 @@ function scrapeCityCouncilURL(){
     scrapeEntryData(4, parties);
     scrapeEntryData('url', urls);
 
-    var citycouncilMembers = [];
     for(var i = 0; i < names.length; i++){
       var result = {};
       result.name = names[i];
@@ -44,20 +45,28 @@ function scrapeCityCouncilURL(){
       result.district = districts[i];
       result.url = "http://council.nyc.gov"+urls[i];
       result.borough = boroughs[i];
-      citycouncilMembers.push(result)
+      citycouncilMembers[districts[i]] = result
     }
 
-    urls.forEach(function(elem){
-      var photo_url = scrapeCouncilMemberPage("http://council.nyc.gov"+elem)
-    })
-    // console.log("names", names);
-    // console.log("districts", districts);
     console.log("citycouncilMembers", citycouncilMembers);
+
+    for(var i =0; i < urls.length; i++){
+      var scraped_data = scrapeCouncilMemberPage("http://council.nyc.gov"+urls[i], i);
+     }
+
+
+     //to do: fix this asyncrhronous stuff
+
+
+
   })
 }
 
-function scrapeCouncilMemberPage(url){
+function scrapeCouncilMemberPage(url, index){
   var email, photo_url, offices = [];
+  var url_split = url.split("http://council.nyc.gov/").join("")
+  var district = url_split.substring(0, 3)
+
   request({url: url}, function(err, response, body){
 
     console.log("scraping council member url:", url)
@@ -67,10 +76,34 @@ function scrapeCouncilMemberPage(url){
     }
 
     var $ = cheerio.load(body);
-    photo_url = $(".inside_top_image img").attr().src
-    console.log("photo url", photo_url);
-    return photo_url
-  })
+    $photo_url = $(".inside_top_image img").attr().src;
+    //to get the proper photo url
+    var photoURLunformatted = $photo_url.split("../../");
+    var photo_url = "http://council.nyc.gov/"+district+"/"+photoURLunformatted[1]
+
+    //still need to scrape this properly
+    var $contactInfo = $(".leftnav .nav_section .nav_text").text();
+
+    //properly formatted
+    var $email = $(".leftnav .nav_section .nav_text a").attr().href.split("mailto:")
+    var email  = $email[1];
+
+    //add to cityCouncilMembers Array
+    citycouncilMembers[districts[index]].photo_url = photo_url;
+    citycouncilMembers[districts[index]].email = email;
+
+    if(index == districts.length - 1){
+      console.log("citycouncilMembers:", citycouncilMembers);
+      var wholeCouncilData  = util.inspect(citycouncilMembers);
+      // fs.writeFile('CityCouncilMembers.js', wholeCouncilData, function(err){
+      //   if(err) throw err;
+      //   console.log('CityCouncilMembers.js is saved');
+      // })
+    }
+    return {
+            photo_url: photo_url,
+            email :  email }
+    })
 }
 
 
